35d0fdf8f43a50f0038c8a502122479a89e0bc22
Alan Q <alanq33@gmail.com>
2012-05-23 10:25:14 +0100

    role player accessible by other roles in ctx

--- lib/role.rb
+++ lib/role.rb
@@ -2,27 +2,30 @@
 module Role
   
   module ClassMethods # to extend role player objects
-  private
-    include ContextAccessor
-    # allow player object instance methods be called on the role's self
-    def method_missing(method, *args, &block)
-      super unless context && context.is_a?(my_context_class)
-      if player.respond_to?(method)
-        player.send(method, *args, &block)
-      else # Neither a role method nor a valid player instance method
-        super
-      end
-    end
-    # returns the player as the substitute for an unrecognised constant that is the role name
+    
+    # a role can find another role's player to send the object as an argument
     def player
       context.role_player[self]
     end
-    def role_name
-      self.to_s.split("::").last
+
+    private
+      include ContextAccessor
+      # allow role name to be used to reference the role's object
+      def method_missing(method, *args, &block)
+        super unless context && context.is_a?(my_context_class)
+        if player.respond_to?(method)
+          player.send(method, *args, &block)
+        else # Neither a role method nor a valid player instance method
+          super
+        end
+      end
+
+      def role_name
+        self.to_s.split("::").last
+      end
+      def my_context_class # a role is defined inside its context class
+        self.to_s.chomp(role_name).constantize
+      end
     end
-    def my_context_class # a role is defined inside its context class
-      self.to_s.chomp(role_name).constantize
-    end
-  end
   extend ClassMethods
 end
